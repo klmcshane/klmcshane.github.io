3d54f8733bda763c73e97b30b23bdb4f2ad23391
User <user@vagelos-ve735-0454.apnd.wlan.private.upenn.edu>
2018-06-05 14:55:29 -0400

    I did a program

---
+++ python/address.py
@@ -0,0 +1,12 @@
+address = {"name": "Kara McShane",
+"department": "Department of English",
+"campus": "Ursinus College",
+"location": "601 E. Main St.",
+"town": "Collegeville, PA ",
+"zip": "19426"}
+
+print(address["name"])
+print(address["department"])
+print(address["campus"])
+print(address["location"])
+print(address["town"] + address["zip"])
