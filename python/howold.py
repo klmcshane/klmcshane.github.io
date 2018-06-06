5c75a412562459ba8af6f2edbe5261b73e3d472a
User <user@vagelos-ve735-0454.apnd.wlan.private.upenn.edu>
2018-06-06 11:14:31 -0400

    Added a new program to count how many people are born in particular centuries

---
+++ python/howold.py
@@ -0,0 +1,22 @@
+birth_dates = {"Harry Potter": 1980,
+"Draco Malfoy": 1980,
+"Albus Potter": 2009,
+"Scorpius Malfoy": 2008,
+"Albus Dumbledore": 1802}
+
+
+nineteen_count = 0
+twenty_count = 0
+twentyone_count = 0
+
+for name, year in birth_dates.items():
+    if year < 1900:
+        nineteen_count += 1
+    elif year < 2000:
+        twenty_count += 1
+    else:
+        twentyone_count += 1
+
+print("There are " + str(nineteen_count) + " 19th c births, "
+    + str(twenty_count) + " 20th c births, "
+    + str(twentyone_count) + " 21st c births in my collection.")
